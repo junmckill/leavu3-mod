ccadb826aeb616009def37f5115cca74f348bd51
Johan Kjölhede <kjolhede@telia.com>
2016-03-12 15:52:12 +0100

    minor cleanups

--- src/main/scala/se/gigurra/leavu3/mfd/Mfd.scala
+++ src/main/scala/se/gigurra/leavu3/mfd/Mfd.scala
@@ -11,10 +11,10 @@ case class Mfd() {
     geo_viewport(at = self.position, viewportSize = 20, heading = self.heading).ppi {
       batched {
 
-        val ul = self.position +(-5f,  5f)
-        val ur = self.position +( 5f,  5f)
-        val ll = self.position +(-5f, -5f)
-        val lr = self.position +( 5f, -5f)
+        val ul = self.position + (-5f,  5f)
+        val ur = self.position + ( 5f,  5f)
+        val ll = self.position + (-5f, -5f)
+        val lr = self.position + ( 5f, -5f)
 
         circle(at = self.position, radius = 10f, color = WHITE)
         circle(at = self.position, radius = 5f, typ = FILL)
