d6de8fa81f98131004ce2cb790c1739e98b0c45f
Johan Kjölhede <kjolhede@telia.com>
2016-04-02 01:48:46 +0200

    cleanup

--- src/main/scala/se/gigurra/leavu3/datamodel/DcsRemoteRemoteConfig.scala
+++ src/main/scala/se/gigurra/leavu3/datamodel/DcsRemoteRemoteConfig.scala
@@ -3,12 +3,12 @@ package se.gigurra.leavu3.datamodel
 import se.gigurra.heisenberg.MapData._
 import se.gigurra.heisenberg.{Parsed, Schema}
 
-case class StaticData(source: SourceData = Map.empty) extends Parsed[StaticData.type] {
+case class DcsRemoteRemoteConfig(source: SourceData = Map.empty) extends Parsed[DcsRemoteRemoteConfig.type] {
   val dlinkSettings = parse(schema.dlinkSettings)
   val missionSettings = parse(schema.missionSettings)
 }
 
-object StaticData extends Schema[StaticData] {
+object DcsRemoteRemoteConfig extends Schema[DcsRemoteRemoteConfig] {
   val dlinkSettings = required[DlinkConfiguration]("dlink-settings", default = DlinkConfiguration())
   val missionSettings = required[MissionConfiguration]("mission-settings", default = MissionConfiguration())
 }
--- src/main/scala/se/gigurra/leavu3/interfaces/DcsRemote.scala
+++ src/main/scala/se/gigurra/leavu3/interfaces/DcsRemote.scala
@@ -2,7 +2,7 @@ package se.gigurra.leavu3.interfaces
 
 import com.twitter.finagle.FailedFastException
 import com.twitter.util.{Await, Future}
-import se.gigurra.leavu3.datamodel.{Configuration, StaticData}
+import se.gigurra.leavu3.datamodel.{Configuration, DcsRemoteRemoteConfig}
 import se.gigurra.leavu3.util.{DefaultTimer, IdenticalRequestPending, RestClient}
 import se.gigurra.serviceutils.json.JSON
 import se.gigurra.serviceutils.twitter.logging.Logging
@@ -27,14 +27,14 @@ case class DcsRemote private(config: Configuration) extends Logging {
       }
   }
 
-  private def initialDownloadConfig(): StaticData = {
+  private def initialDownloadConfig(): DcsRemoteRemoteConfig = {
     try Await.result(downloadConfig()) catch {
       case NonFatal(e) => throw new RuntimeException(s"Failed to communicate with dcs remote!", e)
     }
   }
 
-  private def downloadConfig(): Future[StaticData] = {
-    client.get(s"static-data").map(JSON.read[StaticData])
+  private def downloadConfig(): Future[DcsRemoteRemoteConfig] = {
+    client.get(s"static-data").map(JSON.read[DcsRemoteRemoteConfig])
   }
 
 }
@@ -53,5 +53,5 @@ object DcsRemote {
 
   implicit def remote2client(r: DcsRemote.type): RestClient = instance.client
 
-  def remoteConfig: StaticData = instance.remoteConfig
+  def remoteConfig: DcsRemoteRemoteConfig = instance.remoteConfig
 }
