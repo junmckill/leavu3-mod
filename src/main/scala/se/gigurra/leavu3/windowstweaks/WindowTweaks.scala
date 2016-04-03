33fb75202f12731f2400b5747bd037f73356662f
Johan Kjölhede <kjolhede@telia.com>
2016-04-03 21:12:18 +0200

    WindowTweaks preparations

--- src/main/scala/se/gigurra/leavu3/windowstweaks/WindowTweaks.scala
+++ src/main/scala/se/gigurra/leavu3/windowstweaks/WindowTweaks.scala
@@ -8,70 +8,107 @@ import se.gigurra.serviceutils.twitter.logging.Logging
   */
 object WindowTweaks extends Logging {
 
+  val displayClass = Class.forName("org.lwjgl.opengl.Display")
+  val windowsClass = Class.forName("org.lwjgl.opengl.WindowsDisplay")
+  val display = displayClass.reflectField("display_impl")
+
+  case class Rect(x: Int, y: Int, width: Int, height: Int)
+
   def apply(configuration: Configuration): Unit = {
 
     if (configuration.noFocusOnClick)
       setNeverCaptureFocus()
 
     if (configuration.alwaysOnTop)
-      setAlwaysOnTop(configuration)
+      setAlwaysOnTop()
   }
 
-  private def setNeverCaptureFocus(): Unit = {
+  implicit class RichClass(cls: Class[_]) {
 
-    val displayClass = Class.forName("org.lwjgl.opengl.Display")
-    val windowsClass = Class.forName("org.lwjgl.opengl.WindowsDisplay")
-    val implField = displayClass.getDeclaredField("display_impl")
-    implField.setAccessible(true)
-    val display = implField.get(null)
+    def reflectField(name: String, o: Object = null): Object = {
+      val field = cls.getDeclaredField(name)
+      field.setAccessible(true)
+      field.get(o)
+    }
+
+    def reflectGetter(name: String, o: Object = null): Object = {
+      val method = cls.getDeclaredMethod(name)
+      method.setAccessible(true)
+      method.invoke(o)
+    }
+
+  }
+
+  implicit class RichObjecy(o: Object) {
+    def reflectField(name: String):Object = {
+      o.getClass.reflectField(name, o)
+    }
+
+    def reflectGetter(name: String): Object = {
+      o.getClass.reflectGetter(name, o)
+    }
+
+  }
+
+  def getWindowPosition: Rect = {
+    Rect(
+      display.reflectGetter("getX").asInstanceOf[Int],
+      display.reflectGetter("getY").asInstanceOf[Int],
+      display.reflectGetter("getWidth").asInstanceOf[Int],
+      display.reflectGetter("getHeight").asInstanceOf[Int]
+    )
+  }
+
+  def setNeverCaptureFocus(on: Boolean = true): Unit = {
 
     if (windowsClass.isAssignableFrom(display.getClass)) {
+
       val GWL_EXSTYLE = -20
       val WS_EX_NOACTIVATE = 0x08000000L
+
       val getWindowLongMethod = display.getClass.getDeclaredMethods.find(_.getName == "getWindowLongPtr").get
       val setWindowLongMethod = display.getClass.getDeclaredMethods.find(_.getName == "setWindowLongPtr").get
+
       getWindowLongMethod.setAccessible(true)
       setWindowLongMethod.setAccessible(true)
 
-      val hwndField = display.getClass.getDeclaredField("hwnd")
-      hwndField.setAccessible(true)
-      val hwnd = hwndField.get(display).asInstanceOf[Long]
+      val hwnd = display.reflectField("hwnd").asInstanceOf[Long]
 
       def setWindowLong(index: Int, value: Long): Unit = setWindowLongMethod.invoke(display, hwnd: java.lang.Long, index : java.lang.Integer, value : java.lang.Long)
       def getWindowLong(index: Int): Long = getWindowLongMethod.invoke(display, hwnd: java.lang.Long, index: java.lang.Integer).asInstanceOf[Long]
-      setWindowLong(GWL_EXSTYLE, getWindowLong(GWL_EXSTYLE) | WS_EX_NOACTIVATE)
+
+      val prevStyle = getWindowLong(GWL_EXSTYLE)
+      val withStyle = prevStyle | WS_EX_NOACTIVATE
+      val withoutStyle = withStyle - WS_EX_NOACTIVATE
+
+      setWindowLong(GWL_EXSTYLE, if (on) withStyle else withoutStyle)
+
     } else {
       logger.warning(s"Setting configuration.noFocusOnClick unavailable on this operating system!")
     }
   }
 
-  private def setAlwaysOnTop(configuration: Configuration): Unit = {
-
-    val displayClass = Class.forName("org.lwjgl.opengl.Display")
-    val windowsClass = Class.forName("org.lwjgl.opengl.WindowsDisplay")
-    val implField = displayClass.getDeclaredField("display_impl")
-    implField.setAccessible(true)
-    val display = implField.get(null)
+  def setAlwaysOnTop(on: Boolean = true): Unit = {
 
     if (windowsClass.isAssignableFrom(display.getClass)) {
 
       val SWP_FRAMECHANGED = 0x0020L
       val HWND_TOPMOST = -1L
+      val HWND_BOTTOM = 1L
 
-      val hwndField = display.getClass.getDeclaredField("hwnd")
-      hwndField.setAccessible(true)
-      val hwnd = hwndField.get(display).asInstanceOf[Long]
+      val hwnd = display.reflectField("hwnd").asInstanceOf[Long]
+      val windowPos = getWindowPosition
 
       val setWindowPosMethod = display.getClass.getDeclaredMethods.find(_.getName == "setWindowPos").get
       setWindowPosMethod.setAccessible(true)
       setWindowPosMethod.invoke(
         display,
         hwnd: java.lang.Long,
-        HWND_TOPMOST: java.lang.Long,
-        configuration.x: java.lang.Integer,
-        configuration.y: java.lang.Integer,
-        configuration.width: java.lang.Integer,
-        configuration.height: java.lang.Integer,
+        (if (on) HWND_TOPMOST else HWND_BOTTOM): java.lang.Long,
+        windowPos.x: java.lang.Integer,
+        windowPos.y: java.lang.Integer,
+        windowPos.width: java.lang.Integer,
+        windowPos.height: java.lang.Integer,
         SWP_FRAMECHANGED: java.lang.Long
       )
 
